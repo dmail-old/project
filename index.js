60eb3b2aaaa7fdcebba14d59e044c8a4b92c51e2
Damien Maillard <dmaillard06@gmail.com>
2015-05-12 15:36:43 +0200

    update after env update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-ENV.paths['@dmail/*'] = require('path').dirname(module.dirname) + '/modules/*/index.js'; 
+ENV.metas['@dmail/*'] = {
+	path: module.dirname + '/modules/*/index.js',
+	repository: 'https://github.com/dmail/*.git'
+};
 // maintenant qu'on a set notre environnement on peut démarrer le fichier que l'on souhaite
 
-ENV.import('@dmail/manage').then(function(manage){
-	manage('server');
+ENV.include('@dmail/argv').then(function(manage){
+	//manage('server');
 }, function(error){
 	console.log(error.stack);
 });
