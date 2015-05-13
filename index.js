ccb63c265abac7cf6ddc099af4b9ae5e33238ec6
Damien Maillard <dmaillard06@gmail.com>
2015-05-13 11:12:24 +0200

    update to use manage

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-ENV.metas['@dmail/*'] = {
+ENV.config('@dmail/*', {
 	path: module.dirname + '/modules/*/index.js',
 	repository: 'https://github.com/dmail/*.git'
-};
+});
+
 // maintenant qu'on a set notre environnement on peut démarrer le fichier que l'on souhaite
 
-ENV.include('@dmail/argv').then(function(manage){
+ENV.include('@dmail/manage').then(function(manage){
 	//manage('server');
 }, function(error){
 	console.log(error.stack);
