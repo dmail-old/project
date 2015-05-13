267a0368d4f9368f513c9335a310dc63d2479d85
Damien Maillard <dmaillard06@gmail.com>
2015-05-13 16:18:57 +0200

    minor update

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 ENV.config("@dmail/*", {
 	"path": "./modules/*/index.js",
 	"registry": "git+https://github.com/dmail/*.git",// also support file://, http(s)://
-	"registryBase": "../modules"
 });
 
 // faudrait autre chose pour choisir où le registre git+https met ses modules (par défaut dans modules parent)
