bb7094a0b1db58de82b843aa5b36b8babcf0d017
Damien Maillard <dmaillard06@gmail.com>
2015-05-18 10:01:10 +0200

    registry renamed origin

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 ENV.config("@dmail/*", {
 	"path": "./modules/*/index.js",
-	"registry": "git+https://github.com/dmail/*.git",// also support file://, http(s)://
+	"origin": "git+https://github.com/dmail/*.git",// also support file://, http(s)://
 });
 
 // faudrait autre chose pour choisir où le registre git+https met ses modules (par défaut dans modules parent)
