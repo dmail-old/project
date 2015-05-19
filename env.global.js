d468f6ac05f0e711ac20e0b70ebf7f55861f0c6b
Damien Maillard <dmaillard06@gmail.com>
2015-05-19 17:24:35 +0200

    renamed jsenv + add the main property to metas

--- env.global.js
+++ env.global.js
@@ -1,14 +1,20 @@
 ENV.mainModule = 'index';
-//ENV.baseUrl = './';
-// baseURL is the root by default
-// If I declare baseURL '/client' par exemple
-// comment faire en sorte que certains modules ne soit pas accessible au client?
-// il faudrait deux ENV, un pour le serveur et un pour le client
+ENV.baseURL = new URI('./', ENV.baseURI);
 
-ENV.config('dmail/*', {
-	// "dependencies": ["", ""], // be carefull, setting dependencies here will prevent auto dependency check
-	//"path": "./modules/*/index.js",
-	"path": "github://dmail@*/index.js",
-	"registry": "github://dmail@*",
+ENV.config('github://*', {
+	"path": "modules/github/*"
 });
 
+// https://github.com/systemjs/systemjs/blob/5ed14adca58abd3cf6c29783abd53af00b0c5bff/lib/package.js#L80
+ENV.config('dmail/*', {
+	// "dependencies": ["", ""], // be carefull, setting dependencies here will prevent auto dependency check
+	"main": "index.js",
+	"path": "modules/github/dmail/*",
+	"origin": "github://dmail@*"
+});
+
+/*
+ENV.config('dmail/argv', {
+	"main": "core.js"
+});
+*/
