12663b2c01ea33dbb0baadbe5070ce3b1c92f8a3
Damien Maillard <dmaillard06@gmail.com>
2015-05-22 13:56:39 +0200

    working on config

--- project.env.js
+++ project.env.js
@@ -2,15 +2,18 @@ ENV.mainModule = 'main';
 ENV.baseURL = './';
 ENV.need('./local.env.js');
 
-ENV.config('github://*', {
-	"path": "modules/github/*"
-});
-
 // https://github.com/systemjs/systemjs/blob/5ed14adca58abd3cf6c29783abd53af00b0c5bff/lib/package.js#L80
 ENV.config('dmail/*', {
 	// "dependencies": ["", ""], // be carefull, setting dependencies here will prevent auto dependency check
+	"alias": "github://dmail@*"
+});
+
+ENV.config('github://*', {
 	"main": "index.js",
-	"path": "modules/github/dmail/*",
+	"path": "modules/github/*"
+});
+
+ENV.config('github://dmail@*', {
 	"origin": "github://dmail@*"
 });
 
