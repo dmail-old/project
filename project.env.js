bfc8d23c912f927d1637156a240200fe9540f55e
Damien Maillard <dmaillard06@gmail.com>
2015-05-29 14:25:40 +0200

    update config after jsenv update

--- project.env.js
+++ project.env.js
@@ -2,23 +2,6 @@ jsenv.mainModule = './main';
 jsenv.baseURL = './';
 jsenv.need('./local.env.js');
 
-jsenv.loader.rule('dmail/argv', {
-	// "dependencies": ["", ""], // be carefull, setting dependencies here will prevent auto dependency check
-	to: 'modules/github/dmail/argv/index.js',
-	from: 'github://dmail@argv/index.js'
-});
-
-jsenv.loader.rule('dmail/argv/parse', {
-	to: 'modules/github/dmail/argv/parse.js',
-	from: 'github://dmail@argv/parse.js'
-});
-
-/*
-ENV.config('dmail/argv', {
-	"main": "core.js"
-});
-
-ENV.config('dmail/{name}', {
-	"path": "modules/github/dmail/{name}"
-});
-*/
+jsenv.linkSource('dmail/argv', 'modules/github/dmail/argv', 'index.js');
+jsenv.linkOrigin('dmail/argv', 'github://dmail@argv', 'index.js');
+//jsenv.linkOrigin('modules/github/dmail/argv', 'github://dmail@argv', 'index.js');
