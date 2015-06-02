697e7a3e54ef077088aa454b6ebfa3371eb33a67
Damien Maillard <dmaillard06@gmail.com>
2015-06-02 16:53:52 +0200

    update config

--- project.env.js
+++ project.env.js
@@ -2,6 +2,12 @@ jsenv.mainModule = './main';
 jsenv.baseURL = './';
 jsenv.need('./local.env.js');
 
-jsenv.linkSource('dmail/argv', 'modules/github/dmail/argv', 'index.js');
-jsenv.linkOrigin('dmail/argv', 'github://dmail@argv', 'index.js');
+jsenv.rule('dmail/argv', {
+	main: 'index.js',
+	source: 'modules/github/dmail/argv',
+	origin: 'github://dmail@argv'
+});
+
+//jsenv.linkSource('dmail/argv', 'modules/github/dmail/argv', 'index.js');
+//jsenv.linkOrigin('dmail/argv', 'github://dmail@argv', 'index.js');
 //jsenv.linkOrigin('modules/github/dmail/argv', 'github://dmail@argv', 'index.js');
