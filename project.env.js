571c80129aca55bdd2d2fde1ccc104ced55a3196
Damien Maillard <dmaillard06@gmail.com>
2015-06-03 11:05:22 +0200

    clean config

--- project.env.js
+++ project.env.js
@@ -7,7 +7,3 @@ jsenv.rule('dmail/argv', {
 	source: 'modules/github/dmail/argv',
 	origin: 'github://dmail@argv'
 });
-
-//jsenv.linkSource('dmail/argv', 'modules/github/dmail/argv', 'index.js');
-//jsenv.linkOrigin('dmail/argv', 'github://dmail@argv', 'index.js');
-//jsenv.linkOrigin('modules/github/dmail/argv', 'github://dmail@argv', 'index.js');
