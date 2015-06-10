ae4055d0ca774a300c096629408d416893df6b28
Damien Maillard <dmaillard06@gmail.com>
2015-06-10 14:56:21 +0200

    update to follow jsenv

--- project.env.js
+++ project.env.js
@@ -2,9 +2,17 @@ jsenv.mode = 'install';
 jsenv.mainModule = './main';
 jsenv.baseURL = './';
 jsenv.need('./local.env.js');
+// jsenv.useLoaders.push('css');
 
 jsenv.rule('dmail/argv', {
 	main: 'index.js',
 	source: 'modules/github/dmail/argv',
 	origin: 'github://dmail@argv'
 });
+/*
+jsenv.rule('dmail/manage', {
+	main: 'index.js',
+	source: 'modules/github/dmail/manage',
+	origin: 'github://dmail@manage'
+});
+*/
