1a2fa2272b632e9cf9a7dfba16181f2468d1e89a
Damien Maillard <dmaillard06@gmail.com>
2015-06-29 12:08:01 +0200

    the mainModule must declare it's extension for now

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 jsenv.mode = jsenv.mode || 'install';
 jsenv.baseURL = './';
-jsenv.mainModule = './main';
+jsenv.mainModule = './main.js';
 jsenv.need('./config-project.js');
 jsenv.need('./config-local.js');
 // jsenv.loader.use('css');
