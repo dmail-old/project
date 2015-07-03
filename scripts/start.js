76ba1185ffd794fb62386e4bee93d4a766f6a872
Damien Maillard <dmaillard06@gmail.com>
2015-07-03 12:47:26 +0200

    file server separated from node process

--- scripts/start.js
+++ scripts/start.js
@@ -1,85 +1,83 @@
+var path = require('path');
+var url = require('url');
 var Server = require('../lib/server');
 var sse = require('../lib/sse');
+var createFileResponse = require('jsenv/storages/storage-file').createResponsePromiseForGet;
 var manage = require('../lib/manage');
-var serverUrl = 'http://127.0.0.1:8081';
 
-function listenFilesystemEventStream(){
-	// j'écoute mon propre serveur que je lance juste au dessus -> LOUL
-	// c'est le comportement qu'on auras dans le browser que j'utilise pour node aussi
-	// une autre manière de faire serais d'être notifié en interne que le fichier change pour node
-	// alors que le browser lui passe par le serveur
-	var sourceUrl = serverUrl + '/filesystem-events.js';
-	var source = jsenv.http.createEventSource(serverUrl + '/filesystem-events.js');
+var serverURL = 'http://127.0.0.1:8081';
 
-	source.on('change', function(e){
-		var file = e.data;
-		console.log('the file', file, 'has been modified');
-	});
-
-	console.log('connecting to eventsource :', sourceUrl);
-}
-
-function replaceFileSystemByHttpServer(){
-	var filesystemRoot = jsenv.baseURI;
+function createFileSystemServer(serverUrl){
+	var cwd = process.cwd();
 	var server = Server.create(serverUrl);
 	var filesystemRoom = sse.createRoom();
 
-	return new Promise(function(resolve, reject){
-		server.on('error', function(e){
-			console.log('server error', e);
-			reject(e);
-		});
-
-		server.on('clientError', function(e){
-			console.log('client error', e);
-		});
-
-		server.on('open', function(){
-			console.log('server opened :', serverUrl);
-			filesystemRoom.open();
-			jsenv.chbase(serverUrl);
-			resolve();
-		});
-
-		server.on('close', function(){
-			filesystemRoom.close();
-			console.log('server closed :', serverUrl);
-			jsenv.chbase(filesystemRoot);
-		});
-
-		server.on('request', function(clientRequest, serverResponse){
-			console.log(clientRequest.method, clientRequest.url);
-
-			if( clientRequest.headers && clientRequest.headers.accept === 'text/event-stream' ){
-				var lastEventId = clientRequest.headers['last-event-id'] || new URL(clientRequest.url).searchParams.get('lastEventId');
-
-				filesystemRoom.add(serverResponse, lastEventId);
-				clientRequest.socket.setTimeout(Infinity);
-				clientRequest.connection.on('close', function(){
-					filesystemRoom.remove(serverResponse);
-				});
-			}
-			else{
-				var moduleLocation = new URL('.' + clientRequest.url, filesystemRoot);
-
-				jsenv.store.read(moduleLocation).then(function(response){
-					console.log(response.status, clientRequest.url);
-					serverResponse.writeHead(response.status, response.headers);
-					response.body.pipeTo(serverResponse);
-				}).catch(function(e){
-					//console.log('error during file response creation', e.stack);
-					serverResponse.writeHead(500);
-					serverResponse.end(e.stack);
-				});
-			}
-		});
-
-		server.open();
+	server.on('error', function(e){
+		console.log('server error :', e);
 	});
+
+	server.on('clientError', function(e){
+		console.log('client error :', e);
+	});
+
+	server.on('open', function(){
+		console.log('server opened :', serverUrl);
+		filesystemRoom.open();
+	});
+
+	server.on('close', function(){
+		filesystemRoom.close();
+		console.log('server closed :', serverUrl);
+	});
+
+	server.on('request', function(clientRequest, serverResponse){
+		console.log(clientRequest.method, clientRequest.url);
+
+		if( clientRequest.headers && clientRequest.headers.accept === 'text/event-stream' ){
+			var lastEventId = clientRequest.headers['last-event-id'] || url.parse(clientRequest.url, true).query['lastEventId'];
+
+			filesystemRoom.add(serverResponse, lastEventId);
+			clientRequest.socket.setTimeout(Infinity);
+			clientRequest.connection.on('close', function(){
+				filesystemRoom.remove(serverResponse);
+			});
+		}
+		else{
+			createFileResponse({
+				url: path.resolve(cwd, '.' + url.parse(clientRequest.url).pathname),
+				method: clientRequest.method,
+				headers: clientRequest.headers
+			}).then(function(response){
+				serverResponse.writeHead(response.status, response.headers);
+
+				if( response.body ){
+					response.body.pipeTo(serverResponse);
+				}
+				else{
+					serverResponse.end();
+				}
+			}).catch(function(e){
+				serverResponse.writeHead(500);
+				serverResponse.end(e.stack);
+			});
+		}
+	});
+
+	return server;
 }
 
-require('jsenv');
-jsenv.need('./index.js');
+var server = createFileSystemServer(serverURL);
+var nodeProcess = manage({
+	path: './lib/decorate.js',
+	baseURL: serverURL // tell decorate.js the baseURL is the serverURL
+});
 
-//jsenv.ready(listenFilesystemEventStream);
-jsenv.ready(replaceFileSystemByHttpServer);
+server.on('open', function(){
+	nodeProcess.start();
+});
+
+server.on('close', function(){
+	nodeProcess.stop();
+});
+
+server.open();
